2155ef58ff01a40ce9beee36537911e1db79c412
Dtheran <dtheran1@gmail.com>
2022-10-13 16:48:42 -0500

    implement Hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
-import { categories } from '../../../api/db.json'
 
+const API_URL = 'https://petgram-server-daniel-dtheran1.vercel.app/categories'
 function ListOfCategories () {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed && 'fixed'}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>
         )
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
 export { ListOfCategories }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,9 +1,52 @@
-import styled from 'styled-components'
+import styled, { keyframes, css } from 'styled-components'
+import { fadeIn } from '../../styles/animation'
+
+const bounceDownKeyFrames = keyframes`
+ 0% {
+    top: -70px;
+  }
+
+  25% {
+    top: 0px;
+  }
+
+  40%{
+    top: 10px
+  }
+
+  65%{
+    top: -3px
+  }
+
+  100% {
+    top: 10px;
+  }
+`
+
+export const bounceDown = ({ time = '1s', type = 'ease' } = {}) =>
+  css`
+    animation: ${time} ${bounceDownKeyFrames} ${type};
+  `
 
 export const List = styled.ul`
     display: flex;
     overflow: scroll;
     width: 100%;
+    &.fixed {
+        ${fadeIn()};
+        background: #fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0,0,0, 0.3);
+        left: 0;
+        margin: 0 auto;
+        padding: 5px;
+        max-width: 400px;
+        position: fixed;
+        right: 0;
+        top: -20px;
+        transform: scale(.5);
+        z-index: 1;
+    }
 `
 
 export const Item = styled.li`
