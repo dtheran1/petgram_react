ab2cd91101de826d44224cda22f779c0b5d7ab4c
Dtheran <dtheran1@gmail.com>
2022-10-13 10:27:54 -0500

    add vercel to deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-    <h1>Seguimos configurando el proyecto</h1>,
-    document.getElementById('app')
+  <h1>Seguimos configurando el proyecto</h1>,
+  document.getElementById('app')
 )
