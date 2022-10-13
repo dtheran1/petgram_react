afd6e33c1e41e5061100ee8f08809fe455d68e8c
Dtheran <dtheran1@gmail.com>
2022-10-13 15:28:41 -0500

    animations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
+import { Logo } from './components/Logo'
 
 function App () {
   return (
     <div>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCard />
     </div>
